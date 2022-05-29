3e21ccb2d99c8ef592c8c84e46ec673126b96ed4
Tawan Silva <tawan.tls43@gmail.com>
2022-05-29 15:06:57 -0300

    constructor_this_sobrecarga_encapsulamento

---
+++ constructor_this_sobrecarga_encapsulamento/aula01/Order.java
@@ -0,0 +1,34 @@
+package aula01;
+
+import java.sql.Date;
+
+public class Order {
+	private Date date;
+	private Product product;
+	
+	public Order(Date date, Product product) {
+		super();
+		this.date = date;
+		this.product = product;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	
+	
+	
+}
